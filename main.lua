5109b07d4086429f8a2827f9abdcc6640ab4b8c1
Desmy MJ <desmy.michaelis-joseph@ocsbstudent.ca>
2018-03-06 10:15:30 -0500

    Initial commit

---
+++ main.lua
@@ -0,0 +1,5 @@
+-- Title: Animating Iages
+-- Name: Desmy Michaelis-Joseph
+-- Course: ICS2O/3C
+-- This program displays 3 images moving in different directions,
+--with some coloured text ana d background.
